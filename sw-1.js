d4b96c3abc1c4033152104242fb853e4375ad49f
zadda13 <162315771+zadda13@users.noreply.github.com>
2026-04-19 18:47:06 +0000

    Add files via upload

---
+++ sw-1.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'monmot-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
